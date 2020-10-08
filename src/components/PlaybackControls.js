954606c07099c3ac1900ef55d36a348696fd6387
June Bloom <june.a.bloom@gmail.com>
2020-10-07 18:55:10 -0700

    Add progress bar presentation

--- src/components/PlaybackControls.js
+++ src/components/PlaybackControls.js
@@ -1,6 +1,17 @@
 import { createElement as h } from "react";
 
 function PlaybackControls({ player }) {
+  const fmt = {
+    current: {
+      minutes: (player.currentTime / 60).toFixed(),
+      seconds: (player.currentTime % 60).toFixed().padStart(2, "0"),
+    },
+    duration: {
+      minutes: (player.duration / 60).toFixed(),
+      seconds: (player.duration % 60).toFixed().padStart(2, "0"),
+    },
+  };
+
   return h(
     "div",
     { className: "d-flex" },
@@ -9,7 +20,7 @@ function PlaybackControls({ player }) {
     h(
       "button",
       {
-        className: "button p-05",
+        className: "button button--dark p-05",
         onClick: () => (player.playing ? player.pause() : player.play()),
       },
       player.playing ? "pause" : "play"
@@ -19,16 +30,26 @@ function PlaybackControls({ player }) {
     h(
       "div",
       {
-        className: "flex-grow",
+        className: "progress-bar",
         onClick: (e) => {
           // Normalize the clicked position between [0,1]
-          const { left } = e.target.getBoundingClientRect();
-          const position = (e.clientX - left) / e.target.clientWidth;
-
+          const { left } = e.currentTarget.getBoundingClientRect();
+          const position = (e.clientX - left) / e.currentTarget.clientWidth;
           player.seek(player.duration * position);
         },
       },
-      player.currentTime
+      h("span", {
+        className: "progress-bar__fg",
+        style: { width: `${(player.currentTime / player.duration) * 100}%` },
+      }),
+      h(
+        "span",
+        { className: "progress-bar__text-container" },
+        // current time
+        h("span", {}, `${fmt.current.minutes}:${fmt.current.seconds}`),
+        // remaining time
+        h("span", {}, `${fmt.duration.minutes}:${fmt.duration.seconds}`)
+      )
     ),
 
     // Volume control
