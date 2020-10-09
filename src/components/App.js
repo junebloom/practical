0d98960bda16431d2d48977f3f65a6acaf13f1c4
June Bloom <june.a.bloom@gmail.com>
2020-10-09 10:22:10 -0700

    Move usePlayer hook to separate module

--- src/components/App.js
+++ src/components/App.js
@@ -1,67 +1,9 @@
-import { useRef, useState, useEffect, createElement as h } from "react";
-import { IconContext } from "react-icons";
+import { useState, createElement as h } from "react";
+import usePlayer from "../hooks/usePlayer.js";
 
+import { IconContext } from "react-icons";
 import Recorder from "./Recorder";
 import RecordingsList from "./RecordingsList";
-
-function usePlayer() {
-  const audio = useRef(new Audio());
-  const [selected, setSelected] = useState(null);
-  const [playing, setPlaying] = useState(false);
-  const [currentTime, setCurrentTime] = useState(0);
-  const [duration, setDuration] = useState(0);
-  const [volume, setVolume] = useState(1);
-
-  useEffect(() => {
-    audio.current.onplay = () => {
-      setPlaying(!audio.current.paused);
-    };
-    audio.current.onpause = () => {
-      setPlaying(!audio.current.paused);
-    };
-    audio.current.ontimeupdate = () => {
-      setCurrentTime(audio.current.currentTime);
-    };
-    audio.current.ondurationchange = () => {
-      // Update the duration estimated at record-time with the actual duration
-      const current = audio.current.duration;
-      if (current && current < Infinity) selected.duration = current;
-      // And sync the player state
-      setDuration(selected.duration);
-    };
-    audio.current.onvolumechange = () => {
-      setVolume(audio.current.volume);
-    };
-  }, [selected]);
-
-  // Return the public interface for the player
-  return {
-    selected,
-    playing,
-    currentTime,
-    duration,
-    volume,
-    load: (recording) => {
-      audio.current.src = recording.url;
-      audio.current.load();
-      setSelected(recording);
-      setCurrentTime(0);
-      setDuration(recording.duration);
-    },
-    play: () => {
-      audio.current.play();
-    },
-    pause: () => {
-      audio.current.pause();
-    },
-    seek: (time) => {
-      audio.current.currentTime = time;
-    },
-    setVolume: (value) => {
-      audio.current.volume = value;
-    },
-  };
-}
 
 function App() {
   const [recordings, setRecordings] = useState([]);
---
+++ src/hooks/usePlayer.js
@@ -0,0 +1,62 @@
+import { useRef, useState } from "react";
+
+function usePlayer() {
+  const audio = useRef(new Audio());
+  const [selected, setSelected] = useState(null);
+  const [playing, setPlaying] = useState(false);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(0);
+  const [volume, setVolume] = useState(1);
+
+  useEffect(() => {
+    audio.current.onplay = () => {
+      setPlaying(!audio.current.paused);
+    };
+    audio.current.onpause = () => {
+      setPlaying(!audio.current.paused);
+    };
+    audio.current.ontimeupdate = () => {
+      setCurrentTime(audio.current.currentTime);
+    };
+    audio.current.ondurationchange = () => {
+      // Update the duration estimated at record-time with the actual duration
+      const current = audio.current.duration;
+      if (current && current < Infinity) selected.duration = current;
+      // And sync the player state
+      setDuration(selected.duration);
+    };
+    audio.current.onvolumechange = () => {
+      setVolume(audio.current.volume);
+    };
+  }, [selected]);
+
+  // Return the public interface for the player
+  return {
+    selected,
+    playing,
+    currentTime,
+    duration,
+    volume,
+    load: (recording) => {
+      audio.current.src = recording.url;
+      audio.current.load();
+      setSelected(recording);
+      setCurrentTime(0);
+      setDuration(recording.duration);
+    },
+    play: () => {
+      audio.current.play();
+    },
+    pause: () => {
+      audio.current.pause();
+    },
+    seek: (time) => {
+      audio.current.currentTime = time;
+    },
+    setVolume: (value) => {
+      audio.current.volume = value;
+    },
+  };
+}
+
+export default usePlayer;
