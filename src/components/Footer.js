299286a4e4f98cc8221f722611add59b1b5fe184
June Bloom <june.a.bloom@gmail.com>
2020-10-12 10:07:47 -0700

    Add app name to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,18 +4,20 @@ import { FaGithub } from "react-icons/fa";
 function Footer() {
   return h(
     "footer",
-    { className: "d-flex justify-between align-center mt-1" },
+    { className: "d-flex mt-1" },
+    h("strong", null, "Practical,"),
     h(
       "address",
-      null,
-      "Created by ",
+      { className: "ml-05" },
+      "created by ",
       h("a", { href: "https://github.com/junebloom" }, "June Bloom")
     ),
+    h("span", { className: "flex-grow" }),
     h(
       "a",
       {
         href: "https://github.com/junebloom/practical",
-        className: "d-flex align-center",
+        className: "d-flex",
       },
       "See the source",
       h(FaGithub, { className: "ml-05 text-dark" })
