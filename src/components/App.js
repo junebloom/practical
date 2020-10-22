65ae645a83752d81edcc748feb1216ca8d9c6fe8
June Bloom <june.a.bloom@gmail.com>
2020-10-22 00:26:06 -0700

    Add basic error boundaries

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import useRecordings from "../hooks/useRecordings.js";
 import usePlayer from "../hooks/usePlayer.js";
 
 import { IconContext } from "react-icons";
+import ErrorBoundary from "./ErrorBoundary.js";
 import Recorder from "./Recorder.js";
 import RecordingsList from "./RecordingsList.js";
 import Footer from "./Footer.js";
@@ -17,9 +18,13 @@ function App() {
     h(
       "main",
       { className: "d-flex flex-column maxw-600 minh-full-vh mx-auto p-1" },
-      h(Recorder, { addRecording }),
+      h(ErrorBoundary, null, h(Recorder, { addRecording })),
       h("h2", null, "Recordings"),
-      h(RecordingsList, { recordings, player, deleteRecording }),
+      h(
+        ErrorBoundary,
+        null,
+        h(RecordingsList, { recordings, player, deleteRecording })
+      ),
       h("div", { className: "flex-grow" }),
       h(Footer)
     )
